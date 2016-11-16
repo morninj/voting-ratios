4f2eb94ce86230f200ccf6aafd1b88efd57d7589
Joe Mornin <joe@mornin.org>
2016-11-15 22:40:07 -0800

    Add initial frontend

---
+++ maps.js
@@ -0,0 +1,46 @@
+$('#map1').usmap({
+    stateStyles: {fill: '#efefef', stroke: '#ccc'},
+    stateHoverStyles: {fill: '#3d9943'},
+    stateHoverAnimation: 150,
+    stateSpecificLabelBackingStyles: {
+        'DC': {fill: '#fff', stroke: '#fff'},
+    },
+    stateSpecificLabelBackingHoverStyles: {
+        'DC': {fill: '#fff', stroke: '#fff'},
+    }
+});
+selectState('map1', 'MN');
+
+$(document).ready(function() {
+    $('#map1').on('usmapclick', function(event, data) {
+        if (data.name != 'DC') {
+            selectState('map1', data.name);
+            $('#state_list_1').val(data.name)
+            updateRatio();
+        }
+    });
+});
+
+$('#map2').usmap({
+    stateStyles: {fill: '#efefef', stroke: '#ccc'},
+    stateHoverStyles: {fill: '#3d9943'},
+    stateHoverAnimation: 150,
+    stateSpecificLabelBackingStyles: {
+        'DC': {fill: '#fff', stroke: '#fff'},
+    },
+    stateSpecificLabelBackingHoverStyles: {
+        'DC': {fill: '#fff', stroke: '#fff'},
+    }
+});
+selectState('map2', 'MI');
+
+$(document).ready(function() {
+    $('#map2').on('usmapclick', function(event, data) {
+        if (data.name != 'DC') {
+            selectState('map2', data.name);
+            $('#state_list_2').val(data.name)
+            updateRatio();
+        }
+    });
+});
+
---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $('#state_list_1').on('change', function() {
+        selectState('map1', $('#state_list_1').val());
+        updateRatio();
+        $(this).blur();
+    });
+    $('#state_list_2').on('change', function() {
+        selectState('map2', $('#state_list_2').val());
+        updateRatio();
+        $(this).blur();
+    });
+});
+
+function updateRatio() {
+    var ratio = states[$('#state_list_1').val()][$('#state_list_2').val()];
+    $('.vote-ratio').text(ratio);
+    if (ratio == 1.00) {
+        $('.vote-plural').text('');
+    } else {
+        $('.vote-plural').text('s');
+    }
+
+    $('.map1-metadata > .metadata-entry > .electoral-votes').text(states[$('#state_list_1').val()]['ec_votes']);
+    $('.map2-metadata > .metadata-entry > .electoral-votes').text(states[$('#state_list_2').val()]['ec_votes']);
+    $('.map1-metadata > .metadata-entry > .population').text(states[$('#state_list_1').val()]['population'].toLocaleString());
+    $('.map2-metadata > .metadata-entry > .population').text(states[$('#state_list_2').val()]['population'].toLocaleString());
+    $('.map1-metadata > .metadata-entry > .reg-voters-2014').text(states[$('#state_list_1').val()]['voters2014'].toLocaleString());
+    $('.map2-metadata > .metadata-entry > .reg-voters-2014').text(states[$('#state_list_2').val()]['voters2014'].toLocaleString());
+    $('.map1-metadata > .metadata-entry > .electors-per-voter').text(states[$('#state_list_1').val()]['ecpervoter']);
+    $('.map2-metadata > .metadata-entry > .electors-per-voter').text(states[$('#state_list_2').val()]['ecpervoter']);
+}
+
+function selectState(map, name) {
+    $("#" + map + " > svg > path").each(function() {
+        $(this).css('fill', '');
+    });
+    $("#" + map + " > svg > path#" + name).css('fill', '#32bc3c');
+    updateRatio();
+}
+
